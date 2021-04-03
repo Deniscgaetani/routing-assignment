e3fd5452c0968c597df9238cc23d6817366eb162
DenisGaetani <deniscgaetani@gmail.com>
2021-04-03 01:16:01 -0300

    commit final.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './app/App';
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
